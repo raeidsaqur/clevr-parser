d73354a590bf38bd1fee54fdc61b1d27d677c870
Raeid Saqur <raeidsaqur@cs.toronto.edu>
2020-04-03 07:20:56 -0400

    Test PyPI push completed - pushed at pypi.org/project/clevr-parser/

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import sys
 from setuptools import setup, find_packages
 
-__version__ = '0.1.0'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = '0.0.2'
 url = "https://github.com/raeidsaqur/clevr-parser"
 
-install_requires = []
-setup_requires = ['pytest-runner']
-tests_require = ['pytest', 'pytest-cov']
+install_requires = [
+    'numpy',
+    'scipy',
+    'networkx',
+    'scikit-learn',
+    'scikit-image',
+    'requests'
+]
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+setup_requires = [] + pytest_runner
+tests_require = ['pytest', 'pytest-cov', 'mock']
 
 setup(
     name='clevr_parser',
@@ -13,14 +27,23 @@ setup(
     description='PGM factoring, object candidate proposal generator for CLEVR dataset',
     author='Raeid Saqur',
     author_email='raeidsaqur@cs.toronto.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url=url,
     download_url='{}/archive/{}.tar.gz'.format(url, __version__),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     keywords=[
         'pytorch',
         'CLEVR',
         'language compositionality',
-        'graph-matching'
+        'geometric learning',
+        'graph neural networks'
     ],
+    python_requires='>=3.6',
     install_requires=install_requires,
     setup_requires=setup_requires,
     tests_require=tests_require,
