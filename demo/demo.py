2b054829376e5b7cf7e3edd5f932e543aa771c73
Ameet Deshpande <ameet@nat-oitwireless-inside-vapornet100-10-9-91-9.princeton.edu>
2020-04-15 19:17:41 -0400

    Added examples in demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -49,11 +49,51 @@ def demo_G_scene(parser, gfp):
     G_img = parser.draw_clevr_img_scene_graph(g)
 
 def demo_G_text_spatial_relation(parser, text=None):
-    text = "The sphere is behind a rubber cylinder right of a metal cube" if text is None else text
+    text = "The sphere is behind a rubber cylinder which right of a metal cube" if text is None else text
     q_graph, q_doc = parser.parse(text, return_doc=True)
     ax_title = f"{q_doc}"
     G_text, en_graphs = parser.get_nx_graph_from_doc(q_doc)
     G = parser.draw_graph(G_text, en_graphs, ax_title=ax_title, doc=q_doc)
+
+def get_s_sample(template:str) -> str:
+    # Question samples from [and|or_mat_spa] #
+    """
+    [and_mat_spa_baseline]
+    Final program module = query_color
+    Question type: query, answer: cyan, question for CLEVR_val_011582.png
+    """
+    s_ams_bline = "There is a thing that is on the right side of the tiny cyan rubber thing " \
+                  "and to the left of the large green matte cylinder; what is its color?"
+    """
+    [and_mat_spa_val]
+    Final program module = query_size
+    Question type: query, answer: small, question for CLEVR_val_000019.png,
+    """
+    s_ams_val = "What is the size of the thing that is in front of the big yellow object " \
+                "and is the same shape as the big green thing?"
+    """
+    Final program module = count
+    Question type: count, answer: 2, question for CLEVR_val_008452.png,
+    """
+    s_oms_bline = "How many things are either small green objects in front of the small purple cylinder " \
+                  "or large metallic things that are behind the red matte thing ?"
+    """
+    Final program module = count
+    Question type: count, answer: 2, question for CLEVR_val_000439.png,
+    """
+    s_oms_val = "How many things are cylinders that are behind the large purple metal thing " \
+                "or purple cylinders that are the same size as the cyan thing ?"
+    if template == "and_mat_spa_baseline":
+        return s_ams_bline
+    elif template == "and_mat_spa_val":
+        return s_ams_val
+    elif template == "or_mat_spa_baseline":
+        return s_oms_bline
+    elif template == "or_mat_spa_val":
+        return s_oms_val
+    else:
+        raise ValueError("template must be one of [and|or]_mat_spa_[baseline|val]")
+    return None    
 
 def main():
     clevr_img_name = lambda split, i: f"CLEVR_{split}_{i:06d}.png"
@@ -62,7 +102,8 @@ def main():
     clevrr_baseline_qp = "../data/CLEVRR_v1.0/questions/CLEVRR_compare_baseline_questions.json"
     image_grounding_parsed_gp = "../data/CLEVR_v1.0/scenes_parsed/val_scenes_parsed.json"
 
-    demo_G_text_spatial_relation(parser)
+    s = get_s_sample("and_mat_spa_baseline")
+    demo_G_text_spatial_relation(parser, s)
     print("done")
 
 
